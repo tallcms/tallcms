10903b1d28415f5148b28e84111829365a98cf83
tallcms <hello@tallcms.com>
2026-01-22 21:57:48 +0800

    Bump version to 2.3.0

--- packages/tallcms/cms/config/tallcms.php
+++ packages/tallcms/cms/config/tallcms.php
@@ -10,7 +10,7 @@ return [
     | This is the single source of truth for version comparisons.
     |
     */
-    'version' => '2.1.0',
+    'version' => '2.3.0',
 
     /*
     |--------------------------------------------------------------------------
